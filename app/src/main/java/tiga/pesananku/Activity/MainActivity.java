9ebad5912270eaee27834c6f01d80af5f865b6c5
ditoaugusta <dt_ap@ymail.com>
2015-12-05 15:36:24 +0700

    Update 5 Desember

---
+++ app/src/main/java/tiga/pesananku/Activity/MainActivity.java
@@ -0,0 +1,80 @@
+package tiga.pesananku.Activity;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import tiga.pesananku.R;
+import tiga.pesananku.helper.SQLiteHandler;
+import tiga.pesananku.helper.SessionManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtName;
+    private TextView txtUsername;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtUsername = (TextView) findViewById(R.id.username);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String username = user.get("username");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtUsername.setText(username);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+
+     /* Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table*/
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/tiga/pesananku/app/AppConfig.java
@@ -0,0 +1,12 @@
+package tiga.pesananku.app;
+
+/**
+ * Created by muhammad on 01/12/2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://pesananku.16mb.com/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://pesananku.16mb.com/register.php";
+}
